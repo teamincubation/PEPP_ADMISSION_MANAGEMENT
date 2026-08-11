113f0997883b24168acd692877baedd450c22837
teamincubation <teamincubation@users.noreply.github.com>
2026-08-11 23:43:39 +0530

    test: show database tables

--- test-error.php
+++ test-error.php
@@ -14,8 +14,10 @@ try {
     include 'includes/admin_nav.php';
     echo "<h1>NO ERRORS!</h1>";
     echo "<pre>FINAL SIDEBAR MENU ARRAY:\n";
-    print_r($sidebar_menu);
-    echo "</pre>";
+    $stmt = $pdo->prepare("SHOW TABLES");
+$stmt->execute();
+print_r($stmt->fetchAll(PDO::FETCH_COLUMN));
+exit;
 } catch (Throwable $t) {
     echo "<h1>ERROR:</h1>";
     echo "<pre>" . $t->getMessage() . "\n" . $t->getFile() . ":" . $t->getLine() . "\n" . $t->getTraceAsString() . "</pre>";
