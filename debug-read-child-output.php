a37ef8505921344aa924e673477b04323cb5205a
teamincubation <teamincubation@users.noreply.github.com>
2026-08-11 21:08:31 +0530

    diagnostics: remove CLI check files

--- debug-read-child-output.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-$file = __DIR__ . '/debug-bg-output.txt';
-if (file_exists($file)) {
-    echo file_get_contents($file);
-} else {
-    echo "FILE NOT FOUND";
-}
